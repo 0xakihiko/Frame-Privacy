f2ae77d8b67e3a15d71cfe6ae2749320f218cd01
Matt Holtzman <matt.holtzman@gmail.com>
2022-07-08 14:15:52 +0200

    clean up whitespace, finish rebase

--- main/index.js
+++ main/index.js
@@ -322,10 +322,9 @@ ipcMain.on('*:addFrame', (e, id) => {
 
 // if (process.platform !== 'darwin' && process.platform !== 'win32') app.disableHardwareAcceleration()
 app.on('ready', () => {
-  if (dev) {
+  if (!dev) {
     startUpdater()
   }
-  
 
   data()
   menu()
