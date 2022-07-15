8ddaa728c9ddabb5e50a5aee56ac36f299182174
Akihiko <hello@akihiko.dev>
2022-07-15 16:07:29 +0800

    Changed appid

--- build/notarize.js
+++ build/notarize.js
@@ -5,13 +5,13 @@ const electronNotarize = require('electron-notarize')
 
 module.exports = async function (params) {
   if (process.platform !== 'darwin') return // Only notarize the app on macOS
-  const appId = 'sh.frame.app' // Same appId in electron-builder
+  const appId = 'sh.frame.privacy.app' // Same appId in electron-builder
   const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`)
   if (!fs.existsSync(appPath)) throw new Error(`Cannot find application at: ${appPath}`)
   // Don't notarize if notorization is not turned on
   if(!process.env.NOTARIZE_APP)
   {
-    console.log(`Skipped notirzation of app ${appId}`)
+    console.log(`NOTIRZE_APP environment variable is toggled off, skipped notirzation of app ${appId}`)
     return;
   }
 
